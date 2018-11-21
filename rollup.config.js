1a6ef355d0e9578d69c590887d8730eca0561b8e
Gabriel Rumbaut <grumbaut@wwnorton.com>
2018-11-21 12:53:46 -0500

    refactor: updated rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,40 +2,40 @@ const resolve = require('rollup-plugin-node-resolve');
 const commonjs = require('rollup-plugin-commonjs');
 const babel = require('rollup-plugin-babel');
 
-module.exports = [
-	{
-		input: 'src/photoswiper.js',
-		output: {
-			file: 'dist/photoswiper.browser.js',
-			format: 'umd',
-			sourcemap: true,
-			name: 'photoswiper',
-		},
-		plugins: [
-			resolve(),
-			commonjs(),
-			babel({
-				exclude: 'node_modules/**',
-				presets: ['airbnb'],
-			}),
-		],
+module.exports = [{
+	input: 'src/photoswiper.js',
+	output: {
+		file: 'dist/photoswiper.browser.js',
+		format: 'umd',
+		sourcemap: true,
+		name: 'photoswiper',
 	},
-	{
-		input: 'src/photoswiper.js',
-		output: {
-			file: 'dist/photoswiper.js',
-			format: 'cjs',
-			sourcemap: true,
-		},
-		plugins: [
-			resolve(),
-			commonjs(),
-			babel({
-				exclude: 'node_modules/**',
-				presets: [['airbnb', {
-					targets: { node: 8 },
-				}]],
-			}),
-		],
+	plugins: [
+		resolve(),
+		commonjs(),
+		babel({
+			babelrc: false,
+			exclude: 'node_modules/**',
+			presets: ['airbnb'],
+		}),
+	],
+},
+{
+	input: 'src/photoswiper.js',
+	output: {
+		file: 'dist/photoswiper.js',
+		format: 'cjs',
+		sourcemap: true,
 	},
-];
+	plugins: [
+		resolve(),
+		commonjs(),
+		babel({
+			babelrc: false,
+			exclude: 'node_modules/**',
+			presets: [['airbnb', {
+				targets: { node: 8 },
+			}]],
+		}),
+	],
+}];
